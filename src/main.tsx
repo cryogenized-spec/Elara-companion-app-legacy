fa74d0dca2418c3e38f409b2e88ba1a15a08a84e
cryogenized-spec <cryogenized@gmail.com>
2026-08-29 06:09:25 +0200

    fix: mount chat model controls deterministically

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import { ComposerDraftRecovery } from './components/ComposerDraftRecovery';
 import { ComposerMarkdownAnchor } from './components/ComposerMarkdownAnchor';
 import { ComposerOutboxRecovery } from './components/ComposerOutboxRecovery';
 import { RoutingDiagnosticsPanel } from './components/RoutingDiagnosticsPanel';
+import { ComposerModelSelectorBridge } from './components/ComposerModelSelectorBridge';
 import { installBackgroundSafeAbortBoundary } from './lib/backgroundSafeRuntime';
 import { installMobileViewportSync } from './lib/mobileViewport';
 import './index.css';
@@ -34,5 +35,6 @@ createRoot(document.getElementById('root')!).render(
     <ComposerMarkdownAnchor />
     <ComposerOutboxRecovery />
     <RoutingDiagnosticsPanel />
+    <ComposerModelSelectorBridge />
   </StrictMode>,
 );
