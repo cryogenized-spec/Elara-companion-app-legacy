0701fc5d10cd003f952c80d4ce8fa414805c8617
cryogenized-spec <cryogenized@gmail.com>
2026-08-29 06:08:54 +0200

    fix: remove fragile composer portal bridges

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ import { ComposerDraftRecovery } from './components/ComposerDraftRecovery';
 import { ComposerMarkdownAnchor } from './components/ComposerMarkdownAnchor';
 import { ComposerOutboxRecovery } from './components/ComposerOutboxRecovery';
 import { RoutingDiagnosticsPanel } from './components/RoutingDiagnosticsPanel';
-import { ComposerModelSelectorBridge } from './components/ComposerModelSelectorBridge';
 import { installBackgroundSafeAbortBoundary } from './lib/backgroundSafeRuntime';
 import { installMobileViewportSync } from './lib/mobileViewport';
 import './index.css';
@@ -35,6 +34,5 @@ createRoot(document.getElementById('root')!).render(
     <ComposerMarkdownAnchor />
     <ComposerOutboxRecovery />
     <RoutingDiagnosticsPanel />
-    <ComposerModelSelectorBridge />
   </StrictMode>,
 );
